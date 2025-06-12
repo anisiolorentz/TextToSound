31910b01a044c1f66a68d7c82b63229dac23efeb
Anisio Lorentz <anisioneto18@gmail.com>
2025-06-12 11:44:27 -0300

    interpreter

--- js/Interpreter.js
+++ js/Interpreter.js
@@ -1,5 +1,61 @@
-class Interpreter {
+import { Volume } from "./Volume.js";
+
+export class Interpreter {
+
+    static currentInstrument;
+    static audioContext;
+    static currentNote;
+    static currentOctave;
     // le o caractere e devolve: - o instrumento
     //                           - a nota
-    //vai ter que guardar a oitada atual para poder saber como aumentar ou diminuir
+    //vai ter que guardar a oitava atual para poder saber como aumentar ou diminuir
+    
+    static gmInstruments = {
+    'piano': 'acoustic_grand_piano',
+    'guitarra': 'acoustic_guitar_nylon', 
+    'violino': 'violin',
+    'flauta': 'flute',
+    'trompete': 'trumpet',
+    'baixo': 'acoustic_bass',
+    'bateria': 'synth_drum',
+    'orgao': 'church_organ',
+    'saxofone': 'alto_sax',
+    'clarinete': 'clarinet'
+};
+
+    static async loadInstrument(instrumentName) {
+    if (!this.audioContext) {
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        await this.audioContext.resume(); // Necessário para alguns navegadores
+    }
+    
+    const gmName = this.gmInstruments[instrumentName] || 'flute';
+    
+    try {
+        this.currentInstrument = await Soundfont.instrument(this.audioContext, gmName);
+        console.log(`Instrumento carregado: ${gmName}`);
+        return this.currentInstrument;
+    } catch (error) {
+        console.error('Erro ao carregar instrumento:', error);
+        return null;
+    }
+}
+
+    static playNote(note, duration = 0.5) {
+        if (this.currentInstrument) {
+            const when = this.audioContext.currentTime;
+            const gain = Volume.getGainValue(); // Converter dB para gain
+            
+            this.currentInstrument.play(note, when, { 
+                duration: duration,
+                gain: gain 
+            });
+        }
+    }
+    
+    static playSound(char) {
+        if (char === "a") {
+            this.playNote("A4");
+        }
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { Volume } from "./Volume.js";
+import { Interpreter } from "./Interpreter.js";
 
 const textInput = document.querySelector(".text-input");
 const submitButton = document.querySelector('[type="submit"]');
@@ -19,119 +19,12 @@ fileInput.addEventListener("change", () => { // funcao que lê o arquivo txt ent
     reader.readAsText(file);
 });
 
-// let synth; // objeto synth gera os sons da biblioteca Tone.js
-let currentInstrument;
-let audioContext;
-
-const gmInstruments = {
-    'piano': 'acoustic_grand_piano',
-    'guitarra': 'acoustic_guitar_nylon', 
-    'violino': 'violin',
-    'flauta': 'flute',
-    'trompete': 'trumpet',
-    'baixo': 'acoustic_bass',
-    'bateria': 'synth_drum',
-    'orgao': 'church_organ',
-    'saxofone': 'alto_sax',
-    'clarinete': 'clarinet'
-};
-
-async function loadInstrument(instrumentName) {
-    if (!audioContext) {
-        audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        await audioContext.resume(); // Necessário para alguns navegadores
-    }
-    
-    const gmName = gmInstruments[instrumentName] || 'flute';
-    
-    try {
-        currentInstrument = await Soundfont.instrument(audioContext, gmName);
-        console.log(`Instrumento carregado: ${gmName}`);
-        return currentInstrument;
-    } catch (error) {
-        console.error('Erro ao carregar instrumento:', error);
-        return null;
-    }
-}
-
-function playNote(note, duration = 0.5) {
-    if (currentInstrument) {
-        const when = audioContext.currentTime;
-        const gain = Volume.getGainValue(); // Converter dB para gain
-        
-        currentInstrument.play(note, when, { 
-            duration: duration,
-            gain: gain 
-        });
-    }
-}
-
 submitButton.addEventListener('click', async(event) => {
     event.preventDefault(); //evita que a pagina recarregue (o que impedia o som de tocar)
 
-    if (!currentInstrument) {
-        await loadInstrument('guitarra');
-        // Volume.currentVolume = 0; // Volume inicial
+    if (!Interpreter.currentInstrument) {
+        await Interpreter.loadInstrument("guitarra");
     }
 
-    // Trocar instrumentos
-    if (textInput.value == "piano") {
-        await loadInstrument('piano');
-        playNote('C4');
-    }
-
-    if (textInput.value == "guitarra") {
-        await loadInstrument('guitarra');
-        playNote('A4');
-    }
-
-    if (textInput.value == "violino") {
-        await loadInstrument('violino');
-        playNote('C4');
-    }
-
-    if (textInput.value == "flauta") {
-        await loadInstrument('flauta');
-        console.log(currentInstrument);
-        playNote('C4');
-    }
-
-    if (textInput.value == "bandoneon") {
-        await loadInstrument('bandoneon');
-        playNote('C4');
-        console.log(currentInstrument);
-    }
-
-    if (textInput.value == "baixo") {
-        await loadInstrument('baixo');
-        playNote('C2'); // Baixo uma oitava abaixo
-    }
-
-    if (textInput.value == "bateria") {
-        await loadInstrument('bateria');
-        playNote('C4');
-    }
+    Interpreter.playSound(textInput.value);
 });
-    // esses ifs sao só pra testar o som e o volume
-
-//     if (textInput.value == "a") {
-//         Volume.up(synth);
-//         synth.play("A4");
-//     }
-    
-//     if (textInput.value == "b") {
-//         Volume.down(synth);
-//         synth.play("C4");
-//     }
-
-//     if (textInput.value == "c") {
-//         console.log(Volume.currentVolume);
-//         synth.play("C4", "8n");
-//     }
-
-//     if (textInput.value == "d") { 
-//         console.log(Volume.currentVolume);
-//         Volume.double(synth);
-//         synth.play("C4", "8n");
-//     }
-// });
